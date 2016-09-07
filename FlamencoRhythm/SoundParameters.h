8f3803c650ee248d64fc476f8e9d2b610a3b7d0a
Nirma <nirma.garg@intelliswift.co.in>
2016-09-07 10:46:31 +0530

    project clean up as groups, up down images,sound parameter class

---
+++ FlamencoRhythm/SoundParameters.h
@@ -0,0 +1,13 @@
+//
+//  SoundParameters.h
+//  Making Music
+//
+//  Created by Nirma on 07/09/16.
+//  Copyright © 2016 Intelliswift Software Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundParameters : NSObject
+
+@end
---
+++ FlamencoRhythm/SoundParameters.m
@@ -0,0 +1,13 @@
+//
+//  SoundParameters.m
+//  Making Music
+//
+//  Created by Nirma on 07/09/16.
+//  Copyright © 2016 Intelliswift Software Pvt. Ltd. All rights reserved.
+//
+
+#import "SoundParameters.h"
+
+@implementation SoundParameters
+
+@end
